a1de7fa713a0ec6fbbfd06f6887bbbdf71e25411
karthipasum <karthi@bigziel.com>
2024-09-12 09:17:11 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Home Page!</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam hendrerit
+        erat ac dui porttitor, in semper lorem tempor. Morbi a sapien sapien.
+        Cras vulputate ex et luctus finibus. Donec sit amet semper tellus.
+        Pellentesque odio lacus, hendrerit nec rhoncus sed, varius sed est. Sed
+        lacinia velit ac nisl efficitur mollis et eu urna. Cras volutpat laoreet
+        enim. Donec ipsum sapien, interdum a vehicula ac, luctus nec quam. Ut
+        pulvinar interdum risus, ac mattis ex tempus malesuada. Praesent
+        ullamcorper, leo non venenatis placerat, ligula quam ultricies lectus,
+        vel ullamcorper augue tellus sed dolor. Aenean ultricies, massa in
+        viverra pharetra, sapien dui euismod purus, id aliquet urna nisi eu
+        velit. Mauris vitae dui est. Proin eu est quam. Nam laoreet dolor vel
+        lorem accumsan elementum. Maecenas at libero nulla.
+      </p>
+    </div>
+  );
+}
+
+export default App;
